75cdb3aae9d5a745becf3350ad67b411f280509e
Robin Iderström <Robin.iderstrom@live.se>
2013-11-08 19:08:41 +0100

    asd

---
+++ Assets/Script/Network/Player.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	
+	public string playerName;
+	public NetworkViewID viewID;
+	public NetworkPlayer netPlayer;
+	
+	public bool local = false;
+	
+}
