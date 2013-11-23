a218b74a91882babf72cd91998f6f61344d11cce
Sean <conk3r_xd@hotmail.com>
2013-11-23 16:08:57 +0100

    hej här är projectiler

--- Assets/Script/Seans Projectile Test/Fireball_Projectile.cs
+++ Assets/Script/Seans Projectile Test/Fireball_Projectile.cs
@@ -6,54 +6,26 @@ public class Fireball_Projectile : MonoBehaviour {
 	//directionvektor from player
 	public Vector3 dirVec;
 	public float Speed = 5.0f;
-	public float force = 5.0f;
-	
-	
-	//public mesh eller animation här 
-	public float exploIncrSpeed = 1.0f;
-	public bool isExploding = false;
-	public float exploLifeTime = 0.2f;
-	
+	public GameObject ExploPrefab;
 	
 	// Use this for initialization
 	void Start () {
-	
-		//vad ska vi ha för forcemode på denna?
-		//deltatime?
+		//start movement
 		rigidbody.AddForce(dirVec * Speed);
-		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
-		if(isExploding){
-			//increase size gradually
-			//could make explosion and then remove rigidbody for only visual effects
-			(transform as Transform).localScale += new Vector3(exploIncrSpeed,exploIncrSpeed,exploIncrSpeed);
-		}
-		
 	}
 	
 	
 	void OnCollisionEnter(Collision other){
-		//stop velocity when projectile hit object
-		rigidbody.velocity = Vector3.zero;
-		//enable isExploding, will cause scale to increase (se Update)
-		isExploding = true;
+		//instansiate explosion
+		Instantiate(ExploPrefab, transform.position, transform.rotation);
 
-		if(other.rigidbody){
-			//vi måste sätta eller bestämma hur stor forceVec behöver vara
-			//deltatime?
-			
-			//måste istället ta vectorn från mitten av spheren till 
-			//collisionHit för att bestämma directionVec
-			other.rigidbody.AddForce(dirVec * force, ForceMode.VelocityChange);
-			
-		}
 		//destroy projectile
-		Destroy(gameObject, exploLifeTime);
-		//Destroy(gameObject);
+		Destroy(gameObject);
 	}
 }
 
