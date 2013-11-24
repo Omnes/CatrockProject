565a02f798d8ad750edc461db225e45129952b03
Sean <conk3r_xd@hotmail.com>
2013-11-24 23:58:22 +0100

    explosion

--- Assets/Script/Seans Projectile Test/Fireball_Explosion.cs
+++ Assets/Script/Seans Projectile Test/Fireball_Explosion.cs
@@ -3,12 +3,12 @@ using System.Collections;
 
 public class Fireball_Explosion : MonoBehaviour {
 	
-	//exp
-	public float exploIncrSpeed = 1.0f;
+	
 	public float exploLifeTime = 0.2f;
-	public float force = 5.0f;
-	//the direction that other objects will travel if hit
-	private Vector3 dirVec;
+	public float exploForce = 5.0f;
+	public float exploDamage = 1.0f;
+	
+	private Vector3 dirVec;					//the direction that other objects will travel if hit
 	
 	
 	// Use this for initialization
@@ -18,20 +18,25 @@ public class Fireball_Explosion : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Destroy(this.gameObject, exploLifeTime);
+		//lifetime is here if collsion is not made
+		Destroy(gameObject, exploLifeTime);
 	}
 	
 	void OnCollisionEnter(Collision collisions){
-		//itterates over all collisionspoints for the different collisions
 		
 		if(collisions.rigidbody){
+			//itterates over all collisionspoints for the different collisions
 			foreach(ContactPoint collision in collisions){
-				//räkna ut directionvektorn
+				//creates directionvector for objects
 				dirVec = Vector3.Normalize(collision.point - (collider as SphereCollider).center);
-				collision.otherCollider.rigidbody.AddForce(dirVec * force, ForceMode.Impulse);
-				//if object is an enityt, collisions.sendmessage(DoDamge(hp));
+				collision.otherCollider.rigidbody.AddForce(dirVec * exploForce, ForceMode.Impulse);
+				
+				//if object is an enityt
+				//change this to fit for only entities
+				//collisions.gameObject.GetComponent<Script>().SendMessage("DoDamage", exploDamage);
 			}
 		}
+		Destroy(gameObject);
 	}
 	
 }
