c09dfc0d5147255d5fb41c4d377d9913f851e2b3
Robin Iderström <Robin.iderstrom@live.se>
2014-02-04 21:49:54 +0100

    cleaned up some unnececary code

--- Assets/Script/Network/Player.cs
+++ Assets/Script/Network/Player.cs
@@ -20,13 +20,7 @@ public class Player : System.Object {
 	}
 	
 	public void Instantiate(GameObject prefab, Vector3 pos){
-		GameObject thisPlayerPrefab = prefab;
-		ManageItems itemManager = thisPlayerPrefab.GetComponent<ManageItems>();
-		NetworkItems netItems = GameObject.FindWithTag ("Mastermind").GetComponent<NetworkItems>(); //woah det där kändes inte bra att skriva
-		itemManager.items[0] = netItems.getItem(leftWeaponID);
-		itemManager.items[1] = netItems.getItem(hatID);
-		itemManager.items[2] = netItems.getItem(rightWeaponID);
-		entity = Network.Instantiate(thisPlayerPrefab,pos,Quaternion.identity,0) as GameObject;
+		entity = Network.Instantiate(prefab,pos,Quaternion.identity,0) as GameObject;
 		entity.GetComponent<ManageItems>().setEquips(leftWeaponID,rightWeaponID,hatID);
 	}
 	
