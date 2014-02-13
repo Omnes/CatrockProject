16b31d1ff7dbb43e4f32d1b8d7d1864414063764
Johannes Qvarford <johannes.qvarford@gmail.com>
2014-02-13 20:07:25 +0100

    Basic movement/jump/fall animation.
    Player is in locomotion state until:
    	1. user presses jump button
    	2. player goes from grounded to not grounded
    In case 1, player transitions to run jumping or idle jumping state based
    on current speed. After 80% of the animation has played, it goes back to
    locomotion.
    In case 2, player transitions to falling. If player goes from not
    grounded to grounded, it transitions back to locomotion.

--- Assets/Script/Player/AnimatePlayer.cs
+++ Assets/Script/Player/AnimatePlayer.cs
@@ -4,37 +4,67 @@ using System.Collections;
 using Utility;
 
 public class AnimatePlayer : MonoBehaviour {
-	Animator animator;
-	float syncSpeed = 0;
-
-	void airBegin() {
-		if(animator.GetBool("EndJump") == false) {
-			animator.SetTrigger("BeginJump");
-		}
-	}
-
-	void airEnd() {
-		if(animator.GetBool("BeginJump") == false) {
-			animator.SetTrigger("EndJump");
-		}
-	}
+	private Animator animator;
+	private float syncSpeed = 0;
+	private AnimatorStateInfo curState;
+	private AnimatorStateInfo prevState;
 
 	void Start() {
 		animator = GetComponent<Animator>();
 		if(animator == null) {
 			OurDebug.Log("animator could not be found");
+			Debug.Break();
 		}
+		curState = animator.GetCurrentAnimatorStateInfo(0);
 	}
 
 	void Update() {
 		var speed = networkView.isMine ? rigidbody.velocity.XZ().magnitude : syncSpeed;
-		if(networkView.isMine == false && Input.GetKey(KeyCode.Z)) {
-			OurDebug.Log("speed " + speed);
-		}
 		animator.SetFloat("AbsSpeed", speed);
+
+		updateCurrentAnimationClip();
+		if(isAnimationClipEnd("Base Layer.IdleJumping") || isAnimationClipEnd("Base Layer.RunJumping")) {
+			SendMessage("jumpEnd");
+		}
 	}
 
-	void newVelocity(Vector3 velocity) {
+	public void airBegin() {
+		animator.SetTrigger("AirBegin");
+		animator.SetBool("AirEnd", false);
+	}
+	
+	public void airEnd() {
+		animator.SetTrigger("AirEnd");
+		animator.SetBool("JumpBegin", false);
+		animator.SetBool("AirBegin", false);
+	}
+
+	public void jumpBegin() {
+		animator.SetTrigger("JumpBegin");
+		animator.SetBool("AirEnd", false);
+	}
+
+	public void jumpEnd() {
+		OurDebug.Log("pls work");
+		animator.SetTrigger("AirEnd");
+		animator.SetBool("JumpBegin", false);
+		animator.SetBool("AirBegin", false);
+	}
+
+	bool isAnimationClipBegin(string s) {
+		return curState.IsName(s) && prevState.IsName(s) == false;
+	}
+	
+	bool isAnimationClipEnd(string s) {
+		return prevState.IsName(s) && curState.IsName(s) == false;
+	}
+
+	public void newVelocity(Vector3 velocity) {
 		syncSpeed = velocity.XZ().magnitude;
+	}
+
+	void updateCurrentAnimationClip() {
+		prevState = curState;
+		curState = animator.GetCurrentAnimatorStateInfo(0);
 	}
 }
