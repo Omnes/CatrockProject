3bb67c8756502c3ad76b9704aa80a22129e4f192
Robin Iderström <Robin.iderstrom@live.se>
2014-02-25 01:34:29 +0100

    made a temp icespell and remade "itemhandobjects" (was bored)

--- Assets/Script/Spells/IceSpell.cs
+++ Assets/Script/Spells/IceSpell.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 using System.Collections;
 
 public class IceSpell : Spell {
-
+	public GameObject icewall_prefab;
 	public override void cast(GameObject g, Vector3 mousePos) {
+		Instantiate(icewall_prefab,g.transform.position,Quaternion.identity);
 	}
 }
